cb4e306670f901ccf4b023274c64ed9a7f33e21c
Alexey Batyuta <alexey@batyuta.com>
2022-08-30 09:43:41 +0200

    code reformat

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
 self.addEventListener('push', (event) => {
-  let notification = event.data.json();
-  // const options = {
-  //   body: notification.options.body,
-  //   icon: image
-  // }
-  self.registration.showNotification(
-    notification.title,
-    notification.options
-  );
-  notification = JSON.stringify(notification);
-  console.log(`Service Worker got Notification ${notification}`);
+    let notification = event.data.json();
+    // const options = {
+    //   body: notification.options.body,
+    //   icon: image
+    // }
+    self.registration.showNotification(
+        notification.title,
+        notification.options
+    );
+    notification = JSON.stringify(notification);
+    console.log(`Service Worker got Notification ${notification}`);
 });
