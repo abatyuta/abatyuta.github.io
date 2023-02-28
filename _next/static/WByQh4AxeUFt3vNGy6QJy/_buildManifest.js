4bd4b98e634acfeb0a4a7717120c8fe7f461e923
abatyuta <alexey@batyuta.com>
2023-02-28 08:39:26 +0100

    trying to fix the upload

---
+++ _next/static/WByQh4AxeUFt3vNGy6QJy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/375a85a193efa97a.css","static/chunks/233-5e42e4546922a94c.js","static/chunks/pages/index-14c124c14ad47090.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
